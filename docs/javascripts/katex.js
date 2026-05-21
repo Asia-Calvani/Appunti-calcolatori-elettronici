dbaaca67171b3994334433f56b9d99f5ec447267
Asia Calvani <asia.calvani@gmail.com>
2026-05-21 15:10:22 +0200

    funzi?

--- docs/javascripts/katex.js
+++ docs/javascripts/katex.js
@@ -1,9 +1,13 @@
 // javascripts/katex.js
 document$.subscribe(() => {
+  if (typeof renderMathInElement !== "function") return;
   renderMathInElement(document.body, {
     delimiters: [
+      {left: "\\[", right: "\\]", display: true},
+      {left: "\\(", right: "\\)", display: false},
       {left: "$$", right: "$$", display: true},
-      {left: "$", right: "$", display: false},
-    ]
+      {left: "$", right: "$", display: false}
+    ],
+    ignoredTags: ["script", "noscript", "style", "textarea", "pre", "code"]
   });
 });
